b55bb999fb75e3a9c232c1cd7748c7aea3c8f0a8
Hossein Boustani <eng.boustani@gmail.com>
2019-08-06 09:34:17 +0430

    Adding bootstrap and sass

--- badni/src/App.vue
+++ badni/src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>    
+        </ul>
+      </div>  
+    </nav>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -25,4 +44,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+
+<style lang="scss">
+
 </style>
